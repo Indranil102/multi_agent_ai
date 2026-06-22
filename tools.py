5ae948f0c64bde33a44ddec255c561de37410284
Indranil102e <indranilsamanta2003@gmail.com>
2026-06-22 13:01:41 +0530

    Add tools.py with initial setup for Langchain and Tavily integration

---
+++ tools.py
@@ -0,0 +1,10 @@
+from langchain.tools import tool
+import requests
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+tavily= TavilyClient(api_key=os.getenv("TAVALY_API_KEY"))
